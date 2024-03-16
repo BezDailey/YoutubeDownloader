7fa0084ad92f55beb3381cd0e7ec50b3334af9cc
Jabez Dailey <jabezdailey@icloud.com>
2024-03-16 16:31:28 -0400

    remove mp3

---
+++ download.py
@@ -0,0 +1,15 @@
+import youtube_dl
+
+def download_video(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+download_video('https://www.youtube.com/watch?v=MT0Ta_Qldrs')
